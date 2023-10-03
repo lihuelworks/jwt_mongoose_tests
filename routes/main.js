0daaf4181f7fe11b083bfa8c6394911b52b8a6ce
Mathias Lihuel Gomez <lihuelworks@gmail.com>
2023-10-02 21:10:39 -0300

    moved code to controller

--- routes/main.js
+++ routes/main.js
@@ -8,3 +8,7 @@ const dashboard = async(req, res) => {
     const luckyNumber = Math.floor(Math.random() * 100)
     res.status(200).json({msg: `Hello, John Doe`, secret: `Here is your authorized data, your lucky number is ${luckyNumber}`})
 }
+
+module.exports = {
+    login, dashboard
+}
