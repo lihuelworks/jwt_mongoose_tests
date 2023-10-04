98ecb7c2895dbac114a9b66a8b0dfd89dee56e9c
Mathias Lihuel Gomez <lihuelworks@gmail.com>
2023-10-04 01:23:35 -0300

    uncoupled base errors  from custom-error

---
+++ errors/bad-request.js
@@ -0,0 +1,11 @@
+const CustomAPIError = require('./custom-error')
+
+class BadRequestError extends CustomAPIError {
+    constructor(message) {
+      super(message)
+      this.statusCode = 400
+    }
+  }
+  
+  module.exports = BadRequestError
+  
