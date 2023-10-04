877b2b58d6abbd0ceb1a0dd8773ecd927aefa2ff
Mathias Lihuel Gomez <lihuelworks@gmail.com>
2023-10-04 01:16:35 -0300

    implemented auth middleware and removed lines of redundant code in controllers

--- controllers/main.js
+++ controllers/main.js
@@ -1,6 +1,5 @@
 
 const jwt = require('jsonwebtoken')
-const { JsonWebTokenError } = require('jsonwebtoken')
 const CustomAPIError = require('../errors/custom-error')
 
 const login = async (req, res) => {
@@ -19,22 +18,11 @@ const login = async (req, res) => {
 }
 
 const dashboard = async (req, res) => {
-    const authHeader = req.headers.authorization;
+    console.log("req.user console.log 👉🏻 " , req.user.username)
 
-    if(!authHeader || !authHeader.startsWith('Bearer ')){
-        throw new CustomAPIError('No token provided', 401)
-    }
-
-    const token = authHeader.split(' ')[1]
-    try {
-        const decoded = jwt.verify(token, process.env.JWT_SECRET)
-        console.log(decoded)
-        const luckyNumber = Math.floor(Math.random() * 100)
-        res.status(200).json({ msg: `Hello, ${decoded.username}`, secret: `Here is your authorized data, your lucky number is ${luckyNumber}` })
-    } catch (error) {
-        throw new CustomAPIError('Not authorized to access this route', 401)
-        
-    }
+    const luckyNumber = Math.floor(Math.random() * 100)
+    
+    res.status(200).json({msg: `Hello, ${req.user.username}`, secret: `Here is your authorized data, your lucky number is ${luckyNumber}` })
 }
 
 module.exports = {
