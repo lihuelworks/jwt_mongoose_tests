63544da0a74e877e1ec1195649ace736d237be40
Mathias Lihuel Gomez <lihuelworks@gmail.com>
2023-10-04 01:27:37 -0300

    implemented status codes from http-status-codes into decoupled errors

--- errors/bad-request.js
+++ errors/bad-request.js
@@ -1,11 +1,12 @@
 const CustomAPIError = require('./custom-error')
+const { StatusCodes } = require('http-status-codes')
+
 
 class BadRequestError extends CustomAPIError {
     constructor(message) {
-      super(message)
-      this.statusCode = 400
+        super(message)
+        this.statusCode = StatusCodes.BAD_REQUEST
     }
-  }
-  
-  module.exports = BadRequestError
-  
+}
+
+module.exports = BadRequestError
